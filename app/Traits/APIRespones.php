734ca0a3f6ad75a4aaf506a93369e15709d94e36
fitree <106365006@yru.ac.th>
2021-03-21 16:02:42 +0700

    first commit

---
+++ app/Traits/APIRespones.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\traits;
+
+trait APIRespones {
+    public function filterAndSort($model){
+
+        if (request()->has('q')) {
+            $q = request()->get('q'); // รับค่า q
+            $model = $model->where('title', 'like', '%' . $q . '%'); // select * from posts where title like '%ข้อความค้นหา%';
+
+        } elseif (request()->has('sort_by')) {
+
+            $sortBy = request()->get('sort_by'); // รับค่า sortBy
+            $model = $model->orderBy($sortBy);
+
+        }
+
+        return $model;
+    }
+}
